69ca58a12f8b06c35145aa365b1c2d2177cdd3bc
gajahtml <jmwoo1127@naver.com>
2024-02-22 13:00:29 +0900

    v1.1

--- 03/03.js
+++ 03/03.js
@@ -1,3 +1,14 @@
+
+
+// 함수작성 2: 화살표 함수
+const handleClick = (n) => {
+    document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n}이 눌러졌습니다</h2>`;
+}
+
+
+// 함수 작성 1
+
+/*
 function handleClick(n) {
     // 메세지 영역 가져오기 1
     // document.getElementById("msgArea").innerHTML = "안녕하세요";
@@ -6,4 +17,4 @@ function handleClick(n) {
     // document.querySelector("#msgArea").innerHTML = "<h2>버튼" + n + "이 눌러졌습니다</h2>";
 
     document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n}이 눌러졌습니다</h2>`;
-}
+} */
