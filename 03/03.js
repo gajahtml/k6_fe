fdd8a17b16a42584781ddf2227fe0ce8d083abd6
gajahtml <jmwoo1127@naver.com>
2024-02-22 13:10:19 +0900

    v1.1

--- 03/03.js
+++ 03/03.js
@@ -2,7 +2,11 @@
 
 // 함수작성 2: 화살표 함수
 const handleClick = (n) => {
-    document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n}이 눌러졌습니다</h2>`;
+    let msg;
+    if(n == 1) msg = "<h2>안녕하세요</h2>";
+    else msg = "<h2>안녕히가세요</h2>";
+
+    document.querySelector("#msgArea").innerHTML = msg;
 }
 
 
